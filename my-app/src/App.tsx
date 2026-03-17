2965ef752a162b89f3702518685dc82f083e2081
jayshriVerma <jayshriyog@gmail.com>
2026-03-17 23:35:11 +0000

    custom hook

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,31 @@
-import DataFetcher from "./DataFetcher";
+import useFetch from "./hooks/useFetch";
 import UserList from "./components/UserList";
 import UserTable from "./components/UserTable";
 import UserSummary from "./components/UserSummary";
 
-const App = () => {
-  return (
-    <DataFetcher url="https://jsonplaceholder.typicode.com/users">
-      {({ data: users, loading }) => {
-        if (loading) return <p>Loading...</p>;
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
-        return (
-          <>
-            <UserList users={users} />
-            <UserTable users={users} />
-            <UserSummary totalUsers={users.length} />
-          </>
-        );
-      }}
-    </DataFetcher>
+const App = () => {
+  const {
+    data: users,
+    loading,
+    error,
+  } = useFetch<User[]>("https://jsonplaceholder.typicode.com/users");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  if (!users) return <p>No users found</p>;
+
+  return (
+    <>
+      <UserList users={users} />
+      <UserTable users={users} />
+      <UserSummary totalUsers={users.length} />
+    </>
   );
 };
 
